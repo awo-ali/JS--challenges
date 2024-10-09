9b0efb2bafdd31675e488da50d95af18462e1066
Awo Ali <awo@Awos-MacBook-Air.local>
2024-10-09 20:23:40 +0100

    first commit - minMax function

---
+++ code.js
@@ -0,0 +1,6 @@
+function minMax(arr) {
+    const sortedArray = arr.sort(function(a, b){return a-b});
+    return [sortedArray[0], sortedArray[sortedArray.length-1]];
+    
+  }
+  console.log( minMax([105, 2, 33, 4, 56]));
